9b8404795ce6d045053db679dfb1aca08e64a9cb
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2024-02-04 18:44:40 -0300

    feat: use react query to get tasks

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,49 +1,53 @@
+'use client';
+import { ActionButton } from '@/components/actionButton';
 import { CheckboxComponent } from '@/components/ui/checkbox';
+import { useQuery } from '@tanstack/react-query';
 import { FaPencilAlt, FaPlus, FaTrashAlt } from "react-icons/fa"
 
 
-const tasks = [
-  {
-    id: 1,
-    title: 'teste',
-    isChecked: false,
-    createdAt: '03/02/2021',
-    updatedAt: '03/02/2021',
-  },
-  {
-    id: 2,
-    title: 'testeB dadasdd dasdskdmdmaskd dasdksmakdmasdk kasdmadmksamdak dasdkamdkamsdk dask',
-    isChecked: false,
-    createdAt: '03/02/2021',
-    updatedAt: '03/02/2021',
-  },
-  {
-    id: 3,
-    title: 'testeA',
-    isChecked: true,
-    createdAt: '03/02/2021',
-    updatedAt: '03/02/2021',
-  },
-]
+type Task = {
+  id: number,
+  title: string
+  isChecked: boolean,
+  createdAt: string
+  updatedAt: string
+}
 
 export default function Home() {
+  const getTasks = useQuery<Task[]>({
+    queryKey: ["task-list"],
+    queryFn: async () => {
+      const result: Task[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tasks`).then(
+        (res) => res.json()
+      )
+      return result
+    },
+  });
+
+
+  if(getTasks.isLoading){
+    return <div> Carregando ...</div>
+  }
+
   return (
     <div className="flex h-full max-h-screen flex-col items-center">
       <h1 className="pt-3 font-bold">Lista de tarefas</h1>
       <div className='flex flex-row gap-2 py-2'>
-        <button className=" items-center hover:bg-slate-50">
+        <button className="items-center hover:bg-slate-50">
           <FaPlus color="#005A9C"/>
         </button>
         Adicionar tarefa
       </div>
       <div className="flex flex-col gap-2 max-w-lg">
-        {tasks.map((task) =>(
+        {getTasks.data?.map((task) =>(
           <div 
             className="flex flex-row gap-x-2 items-center bg-slate-50 p-2 rounded-md" 
             key={task.id}
           >
             <div className="flex-none">
-             <CheckboxComponent isChecked={task.isChecked}/>
+             <CheckboxComponent 
+               checked={task.isChecked}
+              />
             </div>
            <div className="flex-1 self-end">
               <p>{task.title}</p>
--- frontend/src/components/ui/checkbox.tsx
+++ frontend/src/components/ui/checkbox.tsx
@@ -3,16 +3,13 @@ import React from 'react'
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FiCheck } from "react-icons/fi"
 
-type CheckboxProps = React.ComponentProps<typeof Checkbox.Root> &  {
-  isChecked: boolean
-}
-
+type CheckboxProps = React.ComponentProps<typeof Checkbox.Root> 
 
 export function CheckboxComponent(props: CheckboxProps){
   return (
       <Checkbox.Root 
        className='flex border bg-white rounded-md items-center justify-center shadow-sm hover:bg-[#EEEEEE] min-w-[25px] min-h-[25px]'
-       checked={props.isChecked}
+       {...props}
       >
       <Checkbox.CheckboxIndicator>
         <FiCheck />
